734b3011a1873cc3d3f785f69ab130fee7c5b6eb
FEB-IRI <felixfebiri343@gmail.com>
2023-10-11 08:40:48 -0700

    header

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,5 @@
 #ifndef main_h
 	#define main_h
-
-
 	int _putchar(char);
 	void print_alphabet(void);
 	void print_alphabet_x10(void);
@@ -16,6 +14,4 @@
 	void print_to_98(int n);
 
 
-	#endif
-
-
+#endif 
